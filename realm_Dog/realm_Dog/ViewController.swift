e2dc3a8a699e7a095f0064bc7f38a3821a7157cd
quangkham <nanmaukquangkham@techfun-myanmar.com>
2020-05-08 15:59:38 +0630

    initial commit and install pod 'RealmSwift'

---
+++ realm_Dog/realm_Dog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  realm_Dog
+//
+//  Created by Quang Kham on 08/05/2020.
+//  Copyright © 2020 Quang Kham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
